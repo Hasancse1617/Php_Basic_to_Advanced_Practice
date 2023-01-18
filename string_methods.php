762663d7defb17c6caf211305aefe02301e5b01f
Hasan Ali <hasancseiu16@gmail.com>
2023-01-18 09:22:11 +0600

    added string_methods.php

---
+++ string_methods.php
@@ -0,0 +1,8 @@
+<?php
+$str = "Hasan Ali";
+echo strlen($str)."\n";
+echo str_word_count($str)."\n";
+echo strrev($str)."\n";
+echo strpos($str,"Ali")."\n";
+echo str_replace("Ali","Biswas",$str)."\n";
+?>
