d10935695bbbf78f0cd839288912773e698d9830
Hasan Ali <hasancseiu16@gmail.com>
2023-01-23 11:23:44 +0600

    added include_require

---
+++ include_require/include_require.php
@@ -0,0 +1,13 @@
+<?php
+   include 'header.php';
+   include 'footer.php';
+
+   require 'header.php';
+   require 'footer.php';
+
+   include_once 'header.php';
+   include_once 'footer.php';
+
+   require_once 'header.php';
+   require_once 'footer.php';
+?>
